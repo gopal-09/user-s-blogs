0e645b67717eb8440226b5a2584f9bc2055c4c59
Gopalreddy <gopalreddy6197@gmail.com>
2023-05-13 12:58:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use('/api/blog', blogRouter)
 // app.use('/api',(req,res)=>{
 // res.send('Welcome')
 // })
-mongoose.connect('mongodb://0.0.0.0:27017/blog', { useNewUrlParser: true }, (err) => {
+mongoose.connect('', { useNewUrlParser: true }, (err) => {
     mongoose.set('strictQuery', true);
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
